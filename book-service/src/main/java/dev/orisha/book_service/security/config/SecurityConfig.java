62570ce6b03db1ef68130cf17a260fce2df78e86
Orisha <orisakwenwokocha1@gmail.com>
2024-08-25 16:21:05 +0100

    refactored user auth and authz protocols for book service

--- book-service/src/main/java/dev/orisha/book_service/security/config/SecurityConfig.java
+++ book-service/src/main/java/dev/orisha/book_service/security/config/SecurityConfig.java
@@ -34,14 +34,16 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         var authenticationFilter = new CustomUsernamePasswordAuthenticationFilter();
+
+        String[] publicEndpoints = PUBLIC_ENDPOINTS.toArray(new String[0]);
         return http
                 .csrf(AbstractHttpConfigurer::disable)
                 .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 .addFilterAt(authenticationFilter, UsernamePasswordAuthenticationFilter.class)
                 .addFilterBefore(authorizationFilter, CustomUsernamePasswordAuthenticationFilter.class)
                 .authorizeHttpRequests(auth -> auth
-                        .requestMatchers(PUBLIC_ENDPOINTS.toArray(new String[0])).permitAll()
-                        .anyRequest().authenticated()
+                                                .requestMatchers(publicEndpoints).permitAll()
+                                                .anyRequest().authenticated()
                 )
                 .sessionManagement(session -> session.sessionCreationPolicy(STATELESS))
                 .build();
